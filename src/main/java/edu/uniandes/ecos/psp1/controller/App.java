01ddc0bbcaba7ff52668e8d56b40a1518cac471d
wilman rincon <wrincon_b@hotmail.com>
2015-02-08 20:40:12 -0500

    cambio del pom.xml

--- src/main/java/edu/uniandes/ecos/psp1/controller/App.java
+++ src/main/java/edu/uniandes/ecos/psp1/controller/App.java
@@ -5,10 +5,52 @@
  */
 package edu.uniandes.ecos.psp1.controller;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
+
+import edu.uniandes.ecos.psp1.view.MainView;
+
 /**
  *
  * @author Administrador
  */
-public class App {
+public class App extends HttpServlet{
     
+    public static void main(String[] args) {
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new App()), "/*");
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        MainView.showHome(req,resp);
+       // MainView.showResults(req,resp,0.0," ",0.0);       
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                
+        try {
+            MainView.showHome(req,resp);
+            //consoleInput(req, resp);
+        } catch (Exception ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
---
+++ src/main/java/edu/uniandes/ecos/psp1/view/MainView.java
@@ -0,0 +1,47 @@
+package edu.uniandes.ecos.psp1.view;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class MainView {
+    public static void showHome(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        
+        PrintWriter pw = resp.getWriter();
+        pw.write("<html>");
+        pw.println("<h1>PSP01 Program!</h1>");
+        
+        pw.write("<form action=\"calc\" method=\"post\"> \n" +
+                "    Numbers to Calc: <input type=\"text\" name=\"calc\">\n" +
+                "    <input type=\"submit\" value=\"Calc\">\n" +
+                "</form> ");
+        pw.write("</html>");
+
+    }
+    
+    public static void showResults(HttpServletRequest req, HttpServletResponse resp, Double stdDev, String values, Double mean)
+            throws ServletException, IOException {
+        resp.getWriter().println("<b>List:</b> "+values +"<br>");
+        resp.getWriter().println("<b>Mean:</b> "+mean + "<br>");
+        resp.getWriter().println("<b>STD: </b> "+stdDev + "<br>");
+    }
+    
+    public static void error(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().println("Error!!!");
+    }
+}
